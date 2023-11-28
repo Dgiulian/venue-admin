6ca470169162d38b03fd793a2ee0cc543a302593
Diego Giuliani <giuliani.diego@gmail.com>
2023-11-28 14:43:12 -0300

    feat: add ingreso page

---
+++ src/app/ingreso/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import WelcomeMessage from "@/components/WelcomeMessage";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { User } from "@/lib/types";
+import { getUserDisplayName } from "@/lib/utils";
+import { useState } from "react";
+
+const FormSchema = z.object({
+  nombre: z.string().trim(),
+});
+
+export default function IngresoPage() {
+  const [user, setUser] = useState<User | null>(null);
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      nombre: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    const nombre = form.getValues("nombre");
+    if (nombre) {
+      console.log("Submit");
+      fetch(`register`, {
+        method: "POST",
+        body: JSON.stringify({ nombre: nombre }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data.user);
+          form.setValue("nombre", "");
+        });
+    }
+  }
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  if (user) {
+    return (
+      <div className="flex h-screen flex-col items-center justify-center ">
+        <WelcomeMessage
+          nombre={getUserDisplayName(user)}
+          mesa={user.mesa ? user.mesa.toString() : ""}
+        />
+      </div>
+    );
+  }
+
+  return (
+    <div className=" flex flex-col items-center justify-center">
+      <h1 className="mt-24 text-4xl">Bienvenido</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="mt-11 w-full  md:max-w-md"
+        >
+          <div className="flex flex-col items-start gap-2 p-4 md:flex-row md:items-end">
+            <div className="flex w-full flex-row items-end gap-2 ">
+              <FormField
+                control={form.control}
+                name="nombre"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Ingrese su nombre</FormLabel>
+                    <FormControl>
+                      <Input
+                        className="flex-1"
+                        placeholder="Nombre"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button className="">Ingresar</Button>
+          </div>
+          {/* <Button type="submit">Registrar</Button> */}
+        </form>
+      </Form>
+    </div>
+  );
+}
+
+function debounce<F extends (...args: any[]) => any>(func: F, waitFor: number) {
+  let timeoutId: ReturnType<typeof setTimeout> | null = null;
+  return (...args: Parameters<F>): void => {
+    console.log(timeoutId);
+    if (timeoutId !== null) {
+      console.log("Timeout cleared");
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => func(...args), waitFor);
+  };
+}
--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import React from "react";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { QrCode } from "lucide-react";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { ReactEventHandler, useState } from "react";
 import QRCode from "react-qr-code";
 
 type QRPageProps = {
@@ -10,6 +15,9 @@ type QRPageProps = {
 
 function QRPage({ searchParams }: QRPageProps) {
   const { value } = searchParams;
+  const [nombre, setNombre] = useState(value ?? "");
+
+  const router = useRouter();
 
   const onImageCownload = () => {
     const svg = document.getElementById("QRCode");
@@ -41,17 +49,42 @@ function QRPage({ searchParams }: QRPageProps) {
     img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
   };
 
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    router.push(`/qr?value=${nombre}`);
+  };
+
   return (
-    <div className="h flex h-screen items-center justify-center">
+    <div className="h mt-32 flex h-screen justify-center">
       {value ? (
         <div className="flex flex-col gap-4">
           <div className="bg-white p-4">
             <QRCode id="QRCode" value={value} />
           </div>
           <Button onClick={() => onImageCownload()}>Descargar QR</Button>
+          <Link href="/qr" className={buttonVariants({ variant: "outline" })}>
+            Volver
+          </Link>
         </div>
       ) : (
-        <p>QR no encontrado</p>
+        <div>
+          <p className="mb-4 text-3xl">Genere su c&oacute;digo de ingreso</p>
+
+          <form onSubmit={handleSubmit}>
+            <div className="flex max-w-md gap-4">
+              <Input
+                className="mr-2 flex-1"
+                placeholder="Ingrese aqu&iacute; su nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+              />
+              <Button className=" flex gap-2" type="submit">
+                <QrCode />
+                Generar
+              </Button>
+            </div>
+          </form>
+        </div>
       )}
     </div>
   );
