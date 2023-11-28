5c86bc46bec8be9d317177619806f937747d74e8
Diego Giuliani <giuliani.diego@gmail.com>
2023-11-28 19:08:34 -0300

    feat: add background image to welcome message

--- src/components/WelcomeMessage.tsx
+++ src/components/WelcomeMessage.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Image from "next/image";
+import imageUrl from "./fondo.jpeg";
 
 type Props = {
   nombre: string;
@@ -9,8 +11,19 @@ function WelcomeMessage({ nombre, mesa }: Props) {
   return (
     <div className="w-full max-w-5xl flex-col items-center justify-between space-y-16 font-mono text-sm md:space-y-28 lg:flex">
       <div className="flex flex-col items-center justify-center space-y-9">
-        <p className="text-4xl uppercase md:text-7xl ">Bienvenido</p>
-        <p className="text-3xl font-bold uppercase md:text-6xl">{nombre}</p>
+        <Image
+          src={imageUrl}
+          alt="Background Image"
+          layout="fill"
+          objectFit="cover"
+          className="absolute"
+          priority
+          quality={100}
+        />
+        {/* <p className="z-2 text-4xl uppercase md:text-7xl ">Bienvenido</p> */}
+        <p className="relative text-3xl font-bold uppercase md:text-6xl">
+          {nombre}
+        </p>
       </div>
 
       {mesa && (
