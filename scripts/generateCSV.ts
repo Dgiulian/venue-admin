9f7f42ff485ccd3820d8c79d047471c80111481b
Diego Giuliani <giuliani.diego@gmail.com>
2023-11-28 14:05:31 -0300

    fix: update c-level list

--- scripts/generateCSV.ts
+++ scripts/generateCSV.ts
@@ -1,5 +1,8 @@
 import csv from "csv-parser";
-import * as fs from "fs";
+import fs from "fs";
+import path from "path";
+import archiver from "archiver";
+
 import * as QRCode from "qrcode";
 
 const readCSVAndGenerateQR = async (filePath: string) => {
@@ -10,14 +13,14 @@ const readCSVAndGenerateQR = async (filePath: string) => {
     .pipe(csv())
     .on("data", async (row) => {
       const name = row["NOMBRE Y APELLIDO (Calfrac)"];
-      names.push(name.replace(",", ""));
+      names.push(name.trim().replace(",", ""));
     })
     .on("end", async () => {
       for (let name of names) {
         const qrString = name; // JSON.stringify(name);
 
         try {
-          const fileName = `qr-${index
+          const fileName = `qr-${(index + 1)
             .toString()
             .padStart(3, "0")}-${name.toLowerCase()}.png`;
           await QRCode.toFile(`./scripts/qrs/${fileName}`, qrString, {
@@ -34,4 +37,83 @@ const readCSVAndGenerateQR = async (filePath: string) => {
     });
 };
 
-readCSVAndGenerateQR("./scripts/invitados.csv");
+const deleteFilesWithExtension = (dirPath: string, extension: string): void => {
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+
+    files.forEach((file) => {
+      if (path.extname(file) === extension) {
+        fs.unlink(path.join(dirPath, file), (err) => {
+          if (err) {
+            console.error("Error deleting file:", err);
+          } else {
+            console.log(`Deleted file: ${file}`);
+          }
+        });
+      }
+    });
+  });
+};
+
+/**
+ * Function to zip all contents of a directory
+ * @param sourceDir - Source directory to zip
+ * @param outPath - Output path of the zip file
+ */
+export function zipDirectory(
+  sourceDir: string,
+  outPath: string,
+): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    // create a file to stream archive data to.
+    const output = fs.createWriteStream(outPath);
+    const archive = archiver("zip", {
+      zlib: { level: 9 }, // Sets the compression level.
+    });
+
+    output.on("close", () => {
+      console.log(archive.pointer() + " total bytes");
+      console.log(
+        "Archiver has been finalized and the output file descriptor has closed.",
+      );
+      resolve();
+    });
+
+    // good practice to catch warnings (ie stat failures and other non-blocking errors)
+    archive.on("warning", (err) => {
+      if (err.code === "ENOENT") {
+        console.warn(err);
+      } else {
+        // throw error
+        reject(err);
+      }
+    });
+
+    // good practice to catch this error explicitly
+    archive.on("error", (err) => {
+      reject(err);
+    });
+
+    // pipe archive data to the output file
+    archive.pipe(output);
+
+    // append files
+    archive.directory(sourceDir, false);
+
+    archive.finalize();
+  });
+}
+
+function main() {
+  deleteFilesWithExtension("./scripts/qrs", ".png");
+  readCSVAndGenerateQR("./scripts/invitados-final.csv");
+  zipDirectory(
+    "./scripts/qrs",
+    "./scripts/qrs-" + new Date().toISOString().split("T")[0] + ".zip",
+  );
+}
+
+main();
--- src/app/sorteo/page.tsx
+++ src/app/sorteo/page.tsx
@@ -14,6 +14,23 @@ const CLevels = [
   "Martinez, Adrian",
   "Radaelli, Damián",
   "Lieste, Gustavo",
+
+  "Elias Ezequiel Gerala",
+  "Günther Jürgensen",
+  "Walter Andrés Vallejos",
+  "Rafael Sanchez Blanco",
+  "Gerala Jorge Nahir",
+  "Emmanuel Schulze",
+  "Gabriel Agustin Mateos",
+  "Silvana Andrea Darnes",
+  "Victoria Macarena Martin",
+  "Romina Gomiero",
+  "Carolina Riquelme",
+  "Ricardo Delgado",
+  "Alejandro Bravo",
+  "Mestre, Sebastian",
+  "Gauvron, Cristian",
+  "Vilches, Joaquin",
 ];
 
 export default async function SorteoPage({ searchParams }: ISorteoPageProps) {
